13e953201d1bb046ba2e9677a3436af6b074b9a8
fzp <519896069@qq.com>
2022-01-27 17:37:32 +0800

    调整登录用户

--- app/http/controllers/api/user.go
+++ app/http/controllers/api/user.go
@@ -2,14 +2,12 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"time"
 	"user/app/business"
 	"user/app/http/params"
 	"user/fzp/helper"
 )
 
 func Info(ctx *gin.Context) {
-	time.Sleep(5 * time.Second)
 	user, _ := ctx.Get("authUser")
 	helper.Ok(ctx, user)
 }
